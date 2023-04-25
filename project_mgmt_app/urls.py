f752d699c3b2d9b131fa27494fdc463c9cdeba26
psvar <paulius.svarauskas@gmail.com>
2023-04-25 12:14:33 +0100

    User see their projects

--- project_mgmt_app/urls.py
+++ project_mgmt_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('myprojects/', views.UserProjects, name='user-projects'),
+    path('myprojects/<int:pk>', views.ProjectDetaillView.as_view(), name='project-detail'),
 ]
--- project_mgmt_app/views.py
+++ project_mgmt_app/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from .models import Project
+from django.views import generic
 
 # Create your views here.
 def index(request):
     projects = Project.objects.all()
-    # project_invoice = Project.invoice.all()
     context = {
         'projects': projects,
     }
     return render(request, 'index.html', context=context)
+
+def UserProjects(request):
+    projects = Project.objects.all().filter(user=request.user)
+    context = {
+        'projects': projects,
+    }
+    return render(request, 'user_projects.html', context=context)
+
+
+class ProjectDetaillView(generic.DetailView):
+    model = Project
+    template_name = 'project_detail.html'
+
